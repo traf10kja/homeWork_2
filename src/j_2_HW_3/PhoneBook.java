beb3504714ea41753e10aba06b552751e6185696
Alexey <traf10@bk.ru>
2021-07-29 11:15:30 +0700

    home work_3

--- src/j_2_HW_3/PhoneBook.java
+++ src/j_2_HW_3/PhoneBook.java
@@ -1,7 +1,23 @@
 package j_2_HW_3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class PhoneBook {
+
+    private HashMap<String, Set<String>> pBook = new HashMap<>();
+
+    public void add(String name, String phoneNumber) {
+        Set<String> phoneN = pBook.getOrDefault(name, new HashSet<>());
+        phoneN.add(phoneNumber);
+        pBook.put(name, phoneN);
+    }
+
+    public Set<String> get(String name) {
+        return pBook.get(name);
+    }
 
 }
 
